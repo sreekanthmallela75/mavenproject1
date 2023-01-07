3632a3a1bc00d820806300396a7fe1eef31f29bd
sreekanthmallela75 <sreekanthmallela75@gmail.com>
2023-01-07 18:22:17 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Main.countCharacter();
+        System.out.println("Hello world!");
+    }
+    public  static  void countCharacter(){
+        String s = "This is sreekanth";
+        int count =0;
+        for (int i=0; i< s.length(); i++){
+            if (s.charAt(i) != ' '){
+
+            }
+        }
+        System.out.println(count);
+    }
+}
